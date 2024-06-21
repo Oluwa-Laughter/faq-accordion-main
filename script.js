9d8e6cb07c3f0a509d2b2dae18949d71e847b987
Oluwa-Laughter <210806073@live.unilag.edu.ng>
2024-06-21 22:30:06 +0100

    update css

---
+++ script.js
@@ -0,0 +1,11 @@
+"use-strict";
+const faqContainer = document.querySelector(".faq-container");
+const openAccBtn = document.querySelector(".");
+const closeAccBtn = document.querySelector(".");
+
+const openAccordion = function () {};
+
+const closeAccordion = function () {};
+faqContainer.addEventListener("click", openAccordion);
+
+faqContainer.addEventListener("click", closeAccordion);
